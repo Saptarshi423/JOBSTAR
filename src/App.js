0abfa1bb8d81b6bc13410aa1722b44b15e002f9f
saptarshi.kattyayan <saptarshi.kattyayan@nordson.com>
2023-07-05 19:58:11 +0530

    React router setup

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
-
+import React from "react";
+import { Landing, Error, Dashboard, Register } from "./pages";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <h1>JOBSTAR</h1>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+        <Route path="landing" element={<Landing />} />
+        <Route path="register" element={<Register />} />
+        <Route path="*" element={<Error />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/pages/Register.js
@@ -0,0 +1,19 @@
+import {useState, useEffect} from 'react'
+import { Logo } from '../components'
+import Wrapper from '../assets/wrappers/RegisterPage'
+
+const initialState = {
+  name: '',
+  email:'',
+  password:'',
+  isMember: true
+}
+
+
+function Register() {
+  return (
+    <div>Register</div>
+  )
+}
+
+export default Register
